d1dce28aa3bccfdcd770193acf2c3724f9b2ff44
Alex Clemmer <clemmer.alexander@gmail.com>
2012-06-20 13:01:40 -0400

    Add cache reading/parsing to BloomParamOpt

--- java/src/edu/jhu/jerboa/counting/BloomParamOpt.java
+++ java/src/edu/jhu/jerboa/counting/BloomParamOpt.java
@@ -2,12 +2,16 @@ package edu.jhu.jerboa.counting;
 
 import java.util.logging.Logger;
 import java.util.Hashtable;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 import ilog.concert.*;
 import ilog.cplex.*;
 
 import edu.jhu.jerboa.util.JerboaProperties;
 import edu.jhu.jerboa.classification.*;
+import edu.jhu.jerboa.util.FileManager;
 
 /**
    @author Alex Clemmer <clemmer.alexander@gmail.com>
@@ -22,9 +26,20 @@ public class BloomParamOpt {
     private long numBits;
     private double kmax;
     private Hashtable<String,Double> weights;
+    private Hashtable<String,Integer> features;
+    private Hashtable<String,String[]> trainInst;
+    private Hashtable<String,Integer> users;
+    private Hashtable<String,Double> labels;
 
     private String outputFilename;
     private boolean coreValsCached;
+    private String featuresCache;
+    private String trainInstCache;
+    private String usersCache;
+    private String labelsCache;
+
+    private final String delimiter = "=-=-=-=";
+
 
     public BloomParamOpt () throws Exception {
 	this.propPrefix = "BloomParamOpt";
@@ -40,6 +55,14 @@ public class BloomParamOpt {
 							 ".outputFilename");
 	this.coreValsCached = JerboaProperties.getBoolean(propPrefix +
 							  ".coreValsCached");
+	this.featuresCache = JerboaProperties.getString(propPrefix +
+							".featuresCache");
+	this.trainInstCache = JerboaProperties.getString(propPrefix +
+							 ".trainInstCache");
+	this.usersCache = JerboaProperties.getString(propPrefix +
+						     ".usersCache");
+	this.labelsCache = JerboaProperties.getString(propPrefix +
+						      ".labelsCache");
     }
     
     public void optimize () {
@@ -51,6 +74,15 @@ public class BloomParamOpt {
 
     public void populateCoreValues () {
 	if (this.coreValsCached) {
+	    try {
+		readAll();
+	    }
+	    catch (IOException err) {
+		System.out.println("COW");
+		System.err.println(err);
+		System.exit(0);
+		// write files here
+	    }
 	    System.out.println("cached");
 	    System.exit(0);
 	    // read cached values
@@ -70,16 +102,88 @@ public class BloomParamOpt {
        generate all of them from scratch. This isn't *always* true, but I
        (aclemmer) didn't bother to figure it out, and simply mandated that
        all of them must be there, or we start over again.
-     */
-    private void readAll () {
-	logger.info("Reading features cache")
-	logger.info("Reading training batches cache")
-	logger.info("Reading users cache")
-	logger.info("Reading labels cache")
+    */
+    private void readAll () throws IOException {
+	logger.info("Reading features cache");
+	this.features = readFeaturesCache();
+	
+	logger.info("Reading training batches cache");
+	this.trainInst = readTrainInstCache ();
+	
+	logger.info("Reading users cache");
+	this.users = readUsersCache();
+	
+	logger.info("Reading labels cache");
+	this.labels = readLabelsCache();
     }
 
-    private void readFeaturesCache () {
-	
+    private Hashtable<String,Integer> readFeaturesCache () throws IOException {
+	String[] lines = getLines(this.featuresCache);
+	Hashtable<String,Integer> featCache = new Hashtable<String,Integer>();
+
+	for (int i = 0; i < lines.length; i++) {
+	    String[] spl = lines[i].split("\t");
+	    featCache.put(spl[0], Integer.parseInt(spl[1]));
+	}
+
+	return featCache;
+    }
+
+    private Hashtable<String,String[]> readTrainInstCache () throws IOException {
+	String[] lines = getLines(this.trainInstCache);
+	Hashtable<String,String[]> trainInstCache =
+	    new Hashtable<String,String[]>();
+
+	for (int i = 0; i < lines.length; i++) {
+	    String[] spl = lines[i].split("\t");
+
+	    if (spl.length > 1) {
+		String[] feats = spl[1].split(this.delimiter);
+		trainInstCache.put(spl[0], feats);
+	    }
+	    else {
+		trainInstCache.put(spl[0], new String[0]);
+	    }
+	}
+
+	return trainInstCache;
+    }
+
+    private Hashtable<String,Integer> readUsersCache () throws IOException {
+	String[] lines = getLines(this.usersCache);
+	Hashtable<String,Integer> usersCache = new Hashtable<String,Integer>();
+
+	for (int i = 0; i < lines.length; i++) {
+	    String[] spl = lines[i].split("\t");
+	    users.put(spl[0], Integer.parseInt(spl[1]));
+	}
+
+	return usersCache;
+    }
+
+    private Hashtable<String,Double> readLabelsCache () throws IOException {
+	String[] lines = getLines(this.labelsCache);
+	Hashtable<String,Double> labelsCache = new Hashtable<String,Double>();
+
+	for(int i = 0; i < lines.length; i++) {
+	    String[] spl = lines[i].split("\t");
+	    labelsCache.put(spl[0], Double.parseDouble(spl[1]));
+	}
+
+	return labelsCache;
+    }
+
+    private String[] getLines (String filename) throws IOException {
+	ArrayList<String> lines = new ArrayList<String>();
+	BufferedReader r = FileManager.getReader(filename);
+
+	String buffer;
+	while ((buffer = r.readLine()) != null) {
+	    lines.add(buffer);
+	}
+	r.close();
+
+	return lines.toArray(new String[0]);
     }
 
     /**
