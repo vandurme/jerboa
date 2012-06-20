593356e3b3ee6bd7192b395dbeaf240cc538212a
Alex Clemmer <clemmer.alexander@gmail.com>
2012-06-20 09:46:45 -0400

    Add skeleton implementation of BloomParamOpt

---
+++ java/src/edu/jhu/jerboa/counting/BloomParamOpt.java
@@ -0,0 +1,14 @@
+package edu.jhu.jerboa.counting;
+
+import ilog.concert.*;
+import ilog.cplex.*;
+
+/**
+   @author Alex Clemmer <clemmer.alexander@gmail.com>
+   @since 2012-5-22
+*/
+public class BloomParamOpt {
+    public static void main(String[] args) {
+	System.out.println("COW");
+    }
+}
