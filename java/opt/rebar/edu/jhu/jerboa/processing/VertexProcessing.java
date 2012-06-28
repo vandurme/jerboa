5415659ddc037918cc3224544b357eac7df0c226
Alex Clemmer <clemmer.alexander@gmail.com>
2012-06-28 08:59:28 -0400

    Fill in skeleton implementation of VertexProcessing
    This commit introduces a good use of reflection to get the label
    attribute out of the person object.

--- java/opt/rebar/edu/jhu/jerboa/processing/VertexProcessing.java
+++ java/opt/rebar/edu/jhu/jerboa/processing/VertexProcessing.java
@@ -1,12 +1,29 @@
 package edu.jhu.jerboa.processing;
 
+import java.util.List;
+import java.lang.reflect.*;
+
 import edu.jhu.hltcoe.rebar.data.access.protobuf.Knowledge.*;
 
 public class VertexProcessing {
-  /**
-     Stub method for development. Method signature WILL change.
-   */
-  public static Object getLabel (Vertex v) {
-    throw new UnsupportedOperationException("not implemented");
+  public static String getLabel (Vertex v, String labelAttr) throws
+    ClassNotFoundException, NoSuchMethodException, IllegalAccessException,
+  InvocationTargetException {
+    // Buckle up. This is going to get ugly quick.
+    
+    Class attrClass =
+      Class.forName("edu.jhu.hltcoe.rebar.data.access.protobuf.Knowledge$" +
+                    labelAttr + "Attribute");
+    
+    PersonInfo pi = v.getPersonInfo();
+
+    Method m1 = pi.getClass().getMethod("get" + labelAttr + "List");
+    List attributeList = (List) m1.invoke(pi, new Object[0]);
+    
+    Object attribute = (GenderAttribute) attributeList.get(0);
+
+    Method m = attrClass.getMethod("get" + labelAttr);
+    String label = m.invoke(attribute, new Object[0]).toString();
+    return label;
   }
 }
